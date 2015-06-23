1b192987ead3b9031e1ed3aaae58bfdc08d352d2
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-23 20:59:34 +0200

    test

--- dev/scripts/github.sh
+++ dev/scripts/github.sh
@@ -14,6 +14,6 @@ repo=https://malteahrens:${GH_TOKEN}@github.com/malteahrens/cordova
 
 git remote set-url origin $repo
 git status
-git add dev
+git add .
 git commit -m "Android release for SDK 22 [ci skip]"
 git push
