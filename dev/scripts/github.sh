4dc87a879d58d18199719563471556f239b09100
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-23 22:04:26 +0200

    test

--- dev/scripts/github.sh
+++ dev/scripts/github.sh
@@ -14,6 +14,7 @@ repo=https://malteahrens:${GH_TOKEN}@github.com/malteahrens/cordova
 
 git remote set-url origin $repo
 git status
+git checkout master
 git add dev/CordovaApp-debug.apk
 git commit -m "Android release for SDK 22 [ci skip]"
 git config --global push.default simple
