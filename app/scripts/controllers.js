c5d350774ac33db5143f158932d9c5c347028c21
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-24 20:44:57 +0200

    Geolocation

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,26 +2,24 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 
-    })
+})
 
 .controller('MapController', function($scope, $ionicLoading) {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiLS1tYWx0ZWFocmVucyIsImEiOiJGU21QX2VVIn0.GVZ36UsnwYc_JfiQ61lz7Q';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            zoom: 12.5,
-            center: [48.14882451158226, 11.451873779296875],
-            style: 'https://www.mapbox.com/mapbox-gl-styles/styles/bright-v7.json',
-            minZoom: 9,
-            maxZoom: 20,
-            interactive: true
-        });
-
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            alert(pos.coords.latitude);
-        });
+    mapboxgl.accessToken = 'pk.eyJ1IjoiLS1tYWx0ZWFocmVucyIsImEiOiJGU21QX2VVIn0.GVZ36UsnwYc_JfiQ61lz7Q';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        zoom: 12.5,
+        center: [48.14882451158226, 11.451873779296875],
+        style: 'https://www.mapbox.com/mapbox-gl-styles/styles/bright-v7.json',
+        minZoom: 9,
+        maxZoom: 20,
+        interactive: true
     });
 
-});
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        alert(pos.coords.latitude);
+    });
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
