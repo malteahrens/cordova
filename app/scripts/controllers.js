8d5705762c5c3266913060918f0a586c091f9b9e
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-25 19:44:50 +0200

    test

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -12,25 +12,6 @@ angular.module('starter.controllers', [])
             interactive: true
         });
 
-        var onSuccess = function(position) {
-            alert('Latitude: '          + position.coords.latitude          + '\n' +
-                'Longitude: '         + position.coords.longitude         + '\n' +
-                'Altitude: '          + position.coords.altitude          + '\n' +
-                'Accuracy: '          + position.coords.accuracy          + '\n' +
-                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                'Heading: '           + position.coords.heading           + '\n' +
-                'Speed: '             + position.coords.speed             + '\n' +
-                'Timestamp: '         + position.timestamp                + '\n');
-        };
-
-// onError Callback receives a PositionError object
-//
-        function onError(error) {
-            alert('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
-        }
-
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 // onSuccess Callback
 //   This method accepts a `Position` object, which contains
@@ -53,7 +34,7 @@ angular.module('starter.controllers', [])
 // Options: throw an error if no update is received every 30 seconds.
 //
         var options = {maximumAge: 0, timeout: 10000, enableHighAccuracy:true};
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+        navigator.geolocation.watchPosition(onSuccess, onError, options);
 })
 
 .controller('MapController', function($scope, $ionicLoading) {
