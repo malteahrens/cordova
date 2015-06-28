6f2fcd3e16a4063beec004d7b32ad6e2028b5049
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-28 12:57:45 +0200

    test

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -46,17 +46,19 @@ angular.module('starter.controllers', [])
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  //
   
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   }
 
-    WifiWizard.getScanResults(function(wifis) {
-        $scope.chats=wifis;
+    $scope.$on('$ionicView.enter', function(e) {
+        WifiWizard.getScanResults(function(wifis) {
+            $scope.chats=wifis;
+        });
     });
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
